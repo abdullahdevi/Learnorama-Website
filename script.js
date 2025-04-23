3c0eab526255d25f8ed98d38223d65cff0a14128
abdullahdevi <abdullahimrandev@gmail.com>
2025-04-23 18:55:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+   
